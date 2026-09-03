c971bcf900172c7d086490e4a177c5a886a6b4d7
FritzV <fritz.viljoen@outlook.com>
2026-09-03 13:23:44 +0200

    wire absence-is-absence-never-a-value through config, canaries, requires

--- test/canaries/db/migrate/20200101000000_canary.rb
+++ test/canaries/db/migrate/20200101000000_canary.rb
@@ -1,5 +1,5 @@
 class Canary < ActiveRecord::Migration[7.0]
   def change
-    add_column :canaries, :thing, :string, null: true
+    add_column :canary_records, :thing, :string, null: true
   end
 end
